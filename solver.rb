bb32b7cb0c75a90b58687426a4e9ee7f54b0e138
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-16 18:16:21 +0100

    Corrected second test case and wrote the reverse code portion to pass it

--- solver.rb
+++ solver.rb
@@ -2,10 +2,11 @@ class Solver
     def factorial(n)
         raise ArgumentError unless n >= 0
         return 1 if n == 0
-        return (1..n).inject(1) { |product, number| product * number }
+        (1..n).inject(1) { |product, number| product * number }
     end
 
     def reverse(word)
         raise ArgumentError unless word.is_a?(String)
+        word.reverse
     end
 end
