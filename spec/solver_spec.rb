f6abc609ce37c928dcaf4908bdf072b933267f49
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-16 17:59:36 +0100

    Added the test case for reverse function

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -4,15 +4,27 @@ require_relative '../solver'
 describe Solver do
   let(:solver) { Solver.new }
 
-  it 'should raise an exception when a negative integer is passed to factorial function' do
-    expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+  describe "#factorial" do
+    it 'should raise an exception when a negative integer is passed to factorial function' do
+      expect { solver.factorial(-1) }.to raise_error(ArgumentError)
+    end
+
+    it 'should return 1 when 0 is passed to factorial function' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'should return 120 when 5 is passed to factorial function' do
+      expect(solver.factorial(5)).to eq(120)
+    end
   end
 
-  it 'should return 1 when 0 is passed to factorial function' do
-    expect(solver.factorial(0)).to eq(1)
-  end
+  describe "#reverse" do
+    it 'should raise an exception when a non string parameter is passed to reverse function' do
+      expect { solver.factorial(12345) }.to raise_error(ArgumentError)
+    end
 
-  it 'should return 120 when 5 is passed to factorial function' do
-    expect(solver.factorial(5)).to eq(120)
+    it 'should return olleh when hello is passed to reverse function' do
+      expect(solver.factorial('hello')).to eq('olleh')
+    end
   end
 end
