7e379ea5b8b322985f665f831351d69267cc9e05
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-16 18:10:45 +0100

    Corrected fist test case and wrote the reverse code portion to pass it

--- solver.rb
+++ solver.rb
@@ -4,4 +4,8 @@ class Solver
         return 1 if n == 0
         return (1..n).inject(1) { |product, number| product * number }
     end
+
+    def reverse(word)
+        raise ArgumentError unless word.is_a?(String)
+    end
 end
