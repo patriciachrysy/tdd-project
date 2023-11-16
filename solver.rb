2c8f7aad679e48928fddd769f1c7c8c58b63103a
PATRICIA CHRISTELLE MANEZEU <p.manezeu@group-activa.com>
2023-11-16 17:55:02 +0100

    Corrected third test case and wrote the fatorial code portion to pass it

--- solver.rb
+++ solver.rb
@@ -2,5 +2,6 @@ class Solver
     def factorial(n)
         raise ArgumentError unless n >= 0
         return 1 if n == 0
+        return (1..n).inject(1) { |product, number| product * number }
     end
 end
